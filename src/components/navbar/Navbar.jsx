c0ce63272f0e027a5302231ec3cae40804d7db16
TomasSorgetti <tomassorgetti456@gmail.com>
2024-07-26 23:22:42 -0300

    Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import styles from "./Navbar.module.css"
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
-            <Link className={styles.logo} to="/">Logo</Link>
-            <div className="nav_links_container">
+            <Link className={styles.logo} to="/">Moka Coffee</Link>
+            <div className={styles.nav_links_container}>
                 <NavLink to="/" className={({ isActive }) => isActive && styles.active_option}>Inicio</NavLink>
                 <NavLink to="/productos" className={({ isActive }) => isActive && styles.active_option}>Productos</NavLink>
-                <NavLink to="/contacto" className={({ isActive }) => isActive && styles.active_option}>Contacto</NavLink>
+                <NavLink to="/contacto" className={styles.contact_button}>Contacto</NavLink>
             </div>
         </nav>
     )
